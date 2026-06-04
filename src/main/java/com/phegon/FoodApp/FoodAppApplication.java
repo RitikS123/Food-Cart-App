f3523eef31908b133005195517be0654e0394592
Ritik <ritiksharmakratos@gmail.com>
2026-06-05 00:03:49 +0530

    completed Email Part

--- src/main/java/com/phegon/FoodApp/FoodAppApplication.java
+++ src/main/java/com/phegon/FoodApp/FoodAppApplication.java
@@ -1,14 +1,40 @@
 package com.phegon.FoodApp;
 
+import com.phegon.FoodApp.email_notification.dtos.NotificationDTO;
+import com.phegon.FoodApp.email_notification.services.NotificationService;
+import com.phegon.FoodApp.enums.NotificationType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@RequiredArgsConstructor
 public class FoodAppApplication {
+
+	private final NotificationService notificationService ;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodAppApplication.class, args);
 		System.out.println("Hello Ritik");
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args ->
+		{
+			NotificationDTO notificationDTO = NotificationDTO.builder()
+					.recipient("ritiksharmaiiit@gmail.com")
+					.subject("Hello Ritik")
+					.body("This is a test email!!!")
+					.type(NotificationType.EMAIL)
+					.build();
+
+			notificationService.sendEmail(notificationDTO);
+		};
 	}
 
 }
